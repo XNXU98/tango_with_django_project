68558c0f58d3135d37658bd6009c40468d26252e
Xiaonan Xu <hello_xxn@163.com>
2021-07-27 21:25:09 +0800

    chapter 3 first commit

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls.resolvers import URLPattern
+from rango import views
+from django.urls import path
+
+app_name = 'rango'
+
+urlpatterns = [
+    path ('',views.index, name='index'),
+]
